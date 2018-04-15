25505e71f6447b4fa0934cc0296d650080e46420
Jakub A. Gramsz <jakub.gramsz@gmail.com>
2018-04-16 00:25:44 +0200

    cont

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,18 @@
+fn abc() -> String {
+    String::from("abc")
+}
+
 #[cfg(test)]
 mod tests {
+    use abc;
+
     #[test]
     fn checkit() {
-        assert_eq!(2+2, 3)
+        assert_eq!(2+2, 4)
+    }
+
+    #[test]
+    fn checkabc() {
+        assert_eq!("abc".to_string(), abc())
     }
 }
